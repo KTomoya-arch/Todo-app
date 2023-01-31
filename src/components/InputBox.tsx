b7e9010e3fa323f43a9491bd283856b76b9f2a89
tomoya-kato-lvgs <tomoya.kato@leverages.jp>
2023-01-31 19:15:52 +0900

    component add

---
+++ src/components/InputBox.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+export const InputBox = () => {
+
+    const [text, setText] = useState('');
+
+    
+
+    return (
+        <div>
+            aiueo
+        </div>
+    )
+}
+
+
+export default InputBox
