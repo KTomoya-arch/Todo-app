1817ea88831791f865b2b57216d5987b1f06df16
KTomoya-arch <kato.eng.dao@gmail.com>
2023-02-02 23:05:02 +0900

    [add]完了機能実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,61 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-import './App.css';
-import InputTodo from './components/InputTodo';
-import TodoArea from './components/TodoArea';
-import DoneArea from './components/DoneArea';
+import "./App.css";
+import InputTodo from "./components/InputTodo";
+import TodoArea from "./components/TodoArea";
+import DoneArea from "./components/DoneArea";
 
 function App() {
-  const [todoText, setTodoText] = useState('');
+  const [todoText, setTodoText] = useState("");
 
-  const [incompleteTodos, setIncompleteTodos] = useState(["テスト","テスト2"]);
+  const [incompleteTodos, setIncompleteTodos] = useState(["テスト", "テスト2"]);
+  const [completeTodos, setCompleteTodos] = useState(["テスト3"]);
 
   const onChangeTodoText = (e: any) => setTodoText(e.target.value);
 
   const onClickAdd = () => {
     const newTodos = [...incompleteTodos, todoText];
     setIncompleteTodos(newTodos);
-    setTodoText('');
+    setTodoText("");
   };
 
   const onClickEdit = (index: any) => {
     alert(index);
-  }
+  };
 
   const onClickDelete = (index: any) => {
     const newTodos = [...incompleteTodos];
-    newTodos.splice(index,1);
+    newTodos.splice(index, 1);
     setIncompleteTodos(newTodos);
-  }
+  };
 
   const onClickComplete = (index: any) => {
     const newIncompleteTodos = [...incompleteTodos];
     newIncompleteTodos.splice(index, 1);
-    
-    const newCompleteTodos = []
-  }
+    setIncompleteTodos(newIncompleteTodos);
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setCompleteTodos(newCompleteTodos);
+  };
+
+  const onClickReturn = () => {
+    alert();
+  };
 
   return (
     <>
       <h1>やることリスト</h1>
-      <InputTodo onChangeTodoText={onChangeTodoText} todoText={todoText} onClickAdd={onClickAdd} />
-      <TodoArea incompleteTodos={incompleteTodos} onClickComplete={onClickComplete} onClickEdit={onClickEdit} onClickDelete={onClickDelete}/>
-      <DoneArea />
+      <InputTodo
+        onChangeTodoText={onChangeTodoText}
+        todoText={todoText}
+        onClickAdd={onClickAdd}
+      />
+      <TodoArea
+        incompleteTodos={incompleteTodos}
+        onClickComplete={onClickComplete}
+        onClickEdit={onClickEdit}
+        onClickDelete={onClickDelete}
+      />
+      <DoneArea completeTodos={completeTodos} onClickReturn={onClickReturn} />
     </>
   );
 }
--- src/components/DoneArea.tsx
+++ src/components/DoneArea.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const DoneArea = (props: any) => {
-
-    
+  const { completeTodos, onClickReturn } = props;
 
   return (
-    <div>
-      
+    <div className="todo-area">
+      <ul>
+        {completeTodos.map((todo: any, index: any) => {
+          return (
+            <div key={todo}>
+              <li>{todo}</li>
+              <button onClick={() => onClickReturn(index)}>戻す</button>
+            </div>
+          );
+        })}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default DoneArea
+export default DoneArea;
